4123aefcb93d1df827c8a8ce66020d3bf052fcf6
Ledzianowski, Mateusz <mledzianowski@psi.pl>
2022-12-11 15:22:47 +0100

    Add League components.

---
+++ src/Http/ThronemasterApi.cs
@@ -0,0 +1,14 @@
+﻿namespace TMLeague.Http
+{
+    public class ThroneMasterApi
+    {
+        private readonly HttpClient _httpClient;
+
+        public ThroneMasterApi(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+
+    }
+}
