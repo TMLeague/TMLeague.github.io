1c1a706a383672439a3fdd63ea4db4d7068f4e29
Imrihil <imrihil@gmail.com>
2023-01-20 14:33:41 +0000

    Deploying to gh-pages from  @ b703faf3b24d462c3a9d25f739f85d0278f6e782 🚀

--- js/app.js
+++ js/app.js
@@ -9,3 +9,11 @@
         $(this).tooltip('dispose');
     });
 }
+
+function copyClipboard(data) {
+    navigator.clipboard.writeText(data);
+}
+
+function scrollToTop() {
+    document.documentElement.scrollTop = 0;
+}
