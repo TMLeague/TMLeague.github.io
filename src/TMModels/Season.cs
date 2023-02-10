6f0a7706fb39b004c52921ebb075115670491287
Ledzianowski, Mateusz <mledzianowski@psi.pl>
2023-02-10 11:27:16 +0100

    Old seasons data.

--- src/TMModels/Season.cs
+++ src/TMModels/Season.cs
@@ -4,7 +4,7 @@ namespace TMModels;
 
 public record Season(
     [property: JsonPropertyName("name")] string Name,
-    [property: JsonPropertyName("startDate")] DateTimeOffset StartDate,
-    [property: JsonPropertyName("endDate")] DateTimeOffset EndDate,
+    [property: JsonPropertyName("startDate")] DateTimeOffset? StartDate,
+    [property: JsonPropertyName("endDate")] DateTimeOffset? EndDate,
     [property: JsonPropertyName("divisions")] string[] Divisions
     );
