feb9aab7119f37c3309fe7393937ba8f5076cdaf
Ledzianowski, Mateusz <mledzianowski@psi.pl>
2023-02-10 12:58:12 +0100

    Added route provider.

---
+++ src/TMLeague/RouteProvider.cs
@@ -0,0 +1,34 @@
+﻿namespace TMLeague;
+
+internal class RouteProvider
+{
+    private const string League = "league";
+    private const string Season = "season";
+    private const string Division = "division";
+
+    public static string GetRoute(string? leagueId = null, string? seasonId = null, string? divisionId = null)
+    {
+        if (leagueId == null)
+            return "";
+
+        if (seasonId == null)
+            return $"{League}/{leagueId}";
+
+        if (divisionId == null)
+            return $"{League}/{leagueId}/{Season}/{seasonId}";
+
+        return $"{League}/{leagueId}/{Season}/{seasonId}/{Division}/{divisionId}";
+    }
+
+    public static string GetLeagueIndexRoute(string leagueId) => 
+        $"{League}/{leagueId}/index";
+
+    public static string GetLeagueSummaryRoute(string leagueId) => 
+        $"{League}/{leagueId}/summary";
+
+    public static string GetLeaguePlayersRoute(string leagueId) => 
+        $"{League}/{leagueId}/players";
+
+    public static string GetLeagueSeasonsRoute(string leagueId) => 
+        $"{League}/{leagueId}/seasons";
+}
