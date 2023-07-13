999f8a4f842f4e1dc48e3861b24bfa45a157becd
Ledzianowski, Mateusz <mledzianowski@psi.pl>
2023-07-13 08:27:23 +0200

    Show all leagues on player view.

--- src/TMApplication/Services/PlayerService.cs
+++ src/TMApplication/Services/PlayerService.cs
@@ -1,67 +1,71 @@
-﻿using Microsoft.Extensions.Logging;
-using TMApplication.Providers;
+﻿using TMApplication.Providers;
 using TMApplication.ViewModels;
+using TMModels;
 
 namespace TMApplication.Services;
 
 public class PlayerService
 {
     private readonly IDataProvider _dataProvider;
-    private readonly ILogger<PlayerService> _logger;
 
-    public PlayerService(IDataProvider dataProvider, ILogger<PlayerService> logger)
+    public PlayerService(IDataProvider dataProvider)
     {
         _dataProvider = dataProvider;
-        _logger = logger;
     }
 
-    public async Task<PlayerLeagueViewModel?> GetPlayerVm(string leagueId, string playerName, CancellationToken cancellationToken = default)
+    public async Task<PlayerViewModel?> GetPlayerVm(string playerName, CancellationToken cancellationToken = default)
     {
-        var league = await _dataProvider.GetLeague(leagueId, cancellationToken);
-        if (league == null)
-            return null;
-
         var player = await _dataProvider.GetPlayer(playerName, cancellationToken);
         if (player == null)
             return null;
 
-        _logger.LogInformation($"Name={player.Name}; Leagues={string.Join(", ", player.Leagues.Values.Select(league1 => $"{league1.LeagueId} ({string.Join(",", league1.Results.Select(division => division.SeasonId))})"))}");
+        var leagues = new List<PlayerLeagueViewModel>();
 
-        if (!player.Leagues.TryGetValue(leagueId, out var playerLeague))
-            return null;
+        foreach (var playerLeague in player.Leagues.Values)
+        {
+            var playerLeagueVm = await GetPlayerLeagueVm(playerLeague, cancellationToken);
+            leagues.Add(playerLeagueVm);
+        }
 
-        var seasons = playerLeague.Results
-            .Select(division => new PlayerSeasonScoreViewModel(
-                division.SeasonId.ToUpper(),
-                division.DivisionId.ToUpper(),
-                division.Result.TotalPoints,
-                division.Result.Wins,
-                division.Result.Cla,
-                division.Result.Supplies,
-                division.Result.PowerTokens,
-                division.Result.MinutesPerMove,
-                division.Result.Moves,
-                division.Result.Houses
-                    .Select(houseResult => new PlayerHouseViewModel(
-                        houseResult.Game,
-                        houseResult.House,
-                        houseResult.IsWinner,
-                        houseResult.Points,
-                        houseResult.BattlePenalty,
-                        houseResult.Strongholds,
-                        houseResult.Castles,
-                        houseResult.Cla,
-                        houseResult.Supplies,
-                        houseResult.PowerTokens,
-                        houseResult.MinutesPerMove,
-                        houseResult.Moves,
-                        houseResult.Stats))
-                    .ToArray(),
-                division.Result.PenaltiesPoints,
-                division.Result.Stats
-            ))
-            .ToArray();
-
-        return new PlayerLeagueViewModel(playerName, seasons, player.GeneratedTime);
+        return new PlayerViewModel(playerName, leagues.ToArray(), player.GeneratedTime);
     }
+
+    private async Task<PlayerLeagueViewModel> GetPlayerLeagueVm(PlayerLeague playerLeague, CancellationToken cancellationToken = default)
+    {
+        var league = await _dataProvider.GetLeague(playerLeague.LeagueId, cancellationToken);
+
+        return new PlayerLeagueViewModel(
+            playerLeague.LeagueId,
+            league?.Name ?? string.Empty,
+            playerLeague.Results.Select(GetPlayerSeasonScoreVm).ToArray());
+    }
+
+    private static PlayerSeasonScoreViewModel GetPlayerSeasonScoreVm(PlayerDivision division) => new(
+        division.SeasonId.ToUpper(),
+        division.DivisionId.ToUpper(),
+        division.Result.TotalPoints,
+        division.Result.Wins,
+        division.Result.Cla,
+        division.Result.Supplies,
+        division.Result.PowerTokens,
+        division.Result.MinutesPerMove,
+        division.Result.Moves,
+        division.Result.Houses.Select(GetPlayerHouseVm).ToArray(),
+        division.Result.PenaltiesPoints,
+        division.Result.Stats);
+
+    private static PlayerHouseViewModel GetPlayerHouseVm(HouseResult houseResult) => new(
+        houseResult.Game,
+        houseResult.House,
+        houseResult.IsWinner,
+        houseResult.Points,
+        houseResult.BattlePenalty,
+        houseResult.Strongholds,
+        houseResult.Castles,
+        houseResult.Cla,
+        houseResult.Supplies,
+        houseResult.PowerTokens,
+        houseResult.MinutesPerMove,
+        houseResult.Moves,
+        houseResult.Stats);
 }
