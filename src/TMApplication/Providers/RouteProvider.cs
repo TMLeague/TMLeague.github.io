bbb2856026f64ec04f59dc40a19fcb8c6e7dec2b
Ledzianowski, Mateusz <mledzianowski@psi.pl>
2024-06-28 11:30:47 +0200

    Added interactions attacks table.

--- src/TMApplication/Providers/RouteProvider.cs
+++ src/TMApplication/Providers/RouteProvider.cs
@@ -45,14 +45,18 @@ public static class RouteProvider
             ? $"{League}/{leagueId}/{Summary}/{tableType?.ToString().ToLower()}/{ScoreTypes.Get(tableType, scoreType)?.ToString().ToLower()}"
             : $"{League}/{leagueId}/{Summary}/{tableType?.ToString().ToLower()}/{ScoreTypes.Get(tableType, scoreType)?.ToString().ToLower()}/{divisionId}";
 
-    public static object GetLeagueInteractionsRoute(string leagueId, string? divisionId = null, TableType? tableType = null, ScoreType? scoreType = null) =>
+    public static object GetLeagueInteractionsRoute(string leagueId, string? divisionId = null, InteractionsType interactionsType = InteractionsType.None, TableType? tableType = null, ScoreType? scoreType = null) =>
         scoreType == null ?
-            (string.IsNullOrEmpty(divisionId) ?
-                $"{League}/{leagueId}/{Interactions}" :
-                $"{League}/{leagueId}/{Interactions}/{TableType.Players.ToString().ToLower()}/{ScoreType.Average.ToString().ToLower()}/{divisionId}") :
-            string.IsNullOrEmpty(divisionId)
-                ? $"{League}/{leagueId}/{Interactions}/{tableType?.ToString().ToLower()}/{ScoreTypes.Get(tableType, scoreType)?.ToString().ToLower()}"
-                : $"{League}/{leagueId}/{Interactions}/{tableType?.ToString().ToLower()}/{ScoreTypes.Get(tableType, scoreType)?.ToString().ToLower()}/{divisionId}";
+            string.IsNullOrEmpty(divisionId) ?
+                interactionsType > InteractionsType.Interactions ?
+                    $"{League}/{leagueId}/{Interactions}/{interactionsType.ToString().ToLower()}" :
+                    $"{League}/{leagueId}/{Interactions}" :
+                $"{League}/{leagueId}/{Interactions}/{interactionsType.ToString().ToLower()}/{TableType.Players.ToString().ToLower()}/{ScoreType.Average.ToString().ToLower()}/{divisionId}" :
+            string.IsNullOrEmpty(divisionId) ?
+                interactionsType > InteractionsType.Interactions ?
+                    $"{League}/{leagueId}/{Interactions}/{interactionsType.ToString().ToLower()}/{tableType?.ToString().ToLower()}/{ScoreTypes.Get(tableType, scoreType)?.ToString().ToLower()}" :
+                    $"{League}/{leagueId}/{Interactions}/{tableType?.ToString().ToLower()}/{ScoreTypes.Get(tableType, scoreType)?.ToString().ToLower()}" :
+                $"{League}/{leagueId}/{Interactions}/{interactionsType.ToString().ToLower()}/{tableType?.ToString().ToLower()}/{ScoreTypes.Get(tableType, scoreType)?.ToString().ToLower()}/{divisionId}";
 
     public static string GetPlayerRoute(string? leagueId, string playerName, PlayerTableType type = PlayerTableType.Seasons, House house = House.Unknown) =>
         leagueId == null ?
